8683fca4673acd7c7f5d915f9026747163b72336
Muhammad Ahmad <ahmaddev545@gmail.com>
2026-08-01 12:15:51 +0500

    Configure global axios base url in main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,6 +6,12 @@ import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { store } from '../src/redux/store.js'
 import { Provider } from 'react-redux'
+import axios from 'axios'
+
+// Global Axios Base URL Setup
+const isLocal = window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1";
+axios.defaults.baseURL = isLocal ? "http://localhost:8000" : "https://fyp-virtual-tryon.vercel.app";
+axios.defaults.withCredentials = true;
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
