48ae5c26a504bafdd49dcb52849b7b66ba9d735b
Muhammad Ahmad <ahmaddev545@gmail.com>
2026-09-03 11:22:26 +0500

    Update chatbot new  model

--- backend/controllers/ChatController.js
+++ backend/controllers/ChatController.js
@@ -71,7 +71,7 @@ export const handleBotChat = async (req, res) => {
 
     const chatCompletion = await groq.chat.completions.create({
       messages: apiMessages,
-      model: "llama3-8b-8192",
+      model: "llama-3.3-70b-versatile",
       temperature: 0.3,
       max_tokens: 300,
     });
